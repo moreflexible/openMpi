af544802355aeb294436b974fceb96a379e224b4
moreflexible <enesbilgin61@gmail.com>
2017-06-23 12:53:01 +0300

    update the name of parameter

--- filter.h
+++ filter.h
@@ -6,5 +6,5 @@ BYTE* ConvertBMPToIntensity(const BYTE* Buffer, int width, int height);
 BYTE* ConvertIntensityToBMP(const BYTE* Buffer, int width, int height, long* newsize);
 BYTE* smoothingParalel(const BYTE *raw_intensity, const short maskSize, const int width, const int height);
 BYTE* smoothingSerial(const BYTE *raw_intensity, const short maskSize, const int width, const int height);
-BYTE * edgeDetectionSerial(const BYTE* intensityBuffer, const int width, const int height, float * aciDegerleri);
-BYTE * edgeDetectionParalel(const BYTE* intensityBuffer, const int width, const int height, float * aciDegerleri);
+BYTE * edgeDetectionSerial(const BYTE* intensityBuffer, const int width, const int height, float * angles);
+BYTE * edgeDetectionParalel(const BYTE* intensityBuffer, const int width, const int height, float * angles);
